bc0ce4cb36f11d384eacac2a25f7f79083351b07
Semyon Puchin <spuchin@samokat.ru>
2026-04-21 14:37:08 +0300

    Updated SOAP API interface

--- python-core/workspaces/api/pep_api/adapters/cbr.py
+++ python-core/workspaces/api/pep_api/adapters/cbr.py
@@ -1,6 +1,6 @@
 from attrs import define
 from suds.client import Client
-from suds.xsd.doctor import Import, ImportDoctor
+from suds.xsd.doctor import ImportDoctor
 
 from pep_api.adapters.common.soap import SOAPAPIClientBase
 from pep_api.schemas.cbr import CBRKeyRateParametersSchema
@@ -9,13 +9,10 @@ from pep_api.schemas.cbr import CBRKeyRateParametersSchema
 @define(slots=False, auto_attribs=True, kw_only=True)
 class CBRSOAPAPIClient(SOAPAPIClientBase):
 	def __attrs_post_init__(self) -> None:
-		import_xml_schema: Import = Import("http://www.w3.org/2001/XMLSchema")
-		import_xml_schema.filter.add("http://web.cbr.ru/")
-		import_xml_schema_doctor: ImportDoctor = ImportDoctor(import_xml_schema)
-
+		self._schema.filter.add("http://web.cbr.ru/")
 		self._client: Client = Client(
 			url="http://www.cbr.ru/DailyInfoWebServ/DailyInfo.asmx?wsdl",
-			doctor=import_xml_schema_doctor,
+			doctor=ImportDoctor(self._schema),
 			retxml=True,
 			headers={"User-Agent": "Mozilla"},
 		)
